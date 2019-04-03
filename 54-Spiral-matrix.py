75aaa1af6c0eb896c019761934b9712324f26229
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2019-04-03 12:54:56 -0700

    Create 54-Spiral-matrix.py

---
+++ 54-Spiral-matrix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        rtn = []
+        while matrix:
+            rtn.extend(matrix.pop(0))
+            if matrix and matrix[0]:
+                for row in matrix:
+                    rtn.append(row.pop())
+            if matrix: 
+                rtn += matrix.pop()[::-1]
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    rtn.append(row.pop(0))
+        return rtn
