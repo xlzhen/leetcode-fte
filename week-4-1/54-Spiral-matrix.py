31bca881fb27853026ae9359b8936153ba7fe790
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2019-04-05 16:22:00 -0700

    Update 54-Spiral-matrix.py

--- week-4-1/54-Spiral-matrix.py
+++ week-4-1/54-Spiral-matrix.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
+        # matrix mutation
         rtn = []
         while matrix:
             rtn.extend(matrix.pop(0))
             if matrix and matrix[0]:
                 for row in matrix:
                     rtn.append(row.pop())
-            if matrix: 
-                rtn += matrix.pop()[::-1]
+            if matrix:
+                rtn.extend(matrix.pop()[::-1])
             if matrix and matrix[0]:
                 for row in matrix[::-1]:
                     rtn.append(row.pop(0))
         return rtn
+            
+        
