0d3f4b734c1dd31b05e43cbd96e45c4c669c1d52
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2019-04-05 12:31:15 -0700

    Update 560-Subarray-Sum-Equals-K.py

--- week-4-1/560-Subarray-Sum-Equals-K.py
+++ week-4-1/560-Subarray-Sum-Equals-K.py
@@ -16,6 +16,33 @@ class Solution(object):
             dict_[presum] += 1
         return ans
             
+
+# DP -  Time Limit Exceeded
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        len_nums = len(nums)
+        dp = [[0 for i in range(len_nums)] for j in range(len_nums)]
+    
+        count = 0
+        
+        for i in range(len_nums):
+            dp[i][i] = nums[i]
+            if dp[i][i] == k:
+                count += 1
+            
+        for i in range(len_nums):
+            for j in range(i + 1, len_nums):
+                dp[i][j] = dp[i][j-1] + nums[j]
+                if dp[i][j] == k:
+                    count += 1
+        
+        return count
+                    
         
                     
         
