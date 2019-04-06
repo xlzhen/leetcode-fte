2990cf3cc87dd01eeb2da34000d5c265c715ffc8
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2019-04-06 16:18:17 -0700

    Update 904-Fruit-Into-Baskets.py

--- week-4-1/904-Fruit-Into-Baskets.py
+++ week-4-1/904-Fruit-Into-Baskets.py
@@ -1,12 +1,17 @@
 class Solution:
     def totalFruit(self, tree):
-        l, nums, res = 0, collections.Counter(), 0
-        for r in range(len(tree)):
-            nums[tree[r]] += 1
+        l = 0
+        nums = collections.Counter()
+        rtn = 0
+        
+        for index, val in enumerate(tree):
+            nums[val] += 1
             while len(nums) > 2:
                 nums[tree[l]] -= 1
                 if not nums[tree[l]]:
                     nums.pop(tree[l])
                 l += 1
-            res = max(res, r - l + 1)
-        return res
+            rtn = max(rtn, index - l + 1)
+        
+        return rtn
+        
