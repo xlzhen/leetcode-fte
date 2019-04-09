2149a66a76ce755c2994c7abe2748ece5818f314
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2019-04-09 08:51:11 -0700

    Create 145-Binary-Tree-Postorder-Traversal.py

---
+++ week-4-8/145-Binary-Tree-Postorder-Traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {integer[]}
+    def postorderTraversal(self, root):
+        traversal, stack = [], [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                # pre-order, right first
+                traversal.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+
+        # reverse result
+        return traversal[::-1]           
+        
